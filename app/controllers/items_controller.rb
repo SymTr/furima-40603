6d2c34a15bdde73d3bdf39b6c8ee4b152691b5bf
SymTr <sayumi.tsuruno@gmail.com>
2024-05-09 16:44:54 +0900

    [update] 新規登録/ログイン失敗しても記録残る
    パスワード以外

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,23 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_detail, :category_id,
+                                 :condition_id, :freight_id, :origin_id,
+                                 :delivery_date_id, :price, :user)
   end
 end
