738a42762f9dba45a7eaa4430d6392d8cc0c5e97
SymTr <sayumi.tsuruno@gmail.com>
2024-05-10 06:39:36 +0900

    [update] Rubocop auto fix done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nick_name, :family_name, :first_name, :family_name_kana,:first_name_kana, :birthday, presence: true
-  validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'must include both letters and numbers'
+  validates :nick_name, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday, presence: true
+  validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'must include both letters and numbers'
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name, :family_name, :first_name_kana, :family_name_kana
   end
