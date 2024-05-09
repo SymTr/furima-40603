54f0a6fa931c6d42b6c6794e0f8e564ee5068606
SymTr <sayumi.tsuruno@gmail.com>
2024-05-09 12:26:30 +0900

    [fix] モデルにてバリデーションの記述修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates [ :nick_name, :family_name, :first_name, :family_name_kana,:first_name_kana, :birthday ], presence: true
+  validates :nick_name, :family_name, :first_name, :family_name_kana,:first_name_kana, :birthday, presence: true
   validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'must include both letters and numbers'
 end
