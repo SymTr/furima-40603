b74c89cfd1c18c7f7417458a97e8750a7acd5888
SymTr <sayumi.tsuruno@gmail.com>
2024-05-09 17:59:02 +0900

    [update] 名前・カナとも全角のみ可能

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   validates :nick_name, :family_name, :first_name, :family_name_kana,:first_name_kana, :birthday, presence: true
   validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'must include both letters and numbers'
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name, :family_name, :first_name_kana, :family_name_kana
+  end
 end
