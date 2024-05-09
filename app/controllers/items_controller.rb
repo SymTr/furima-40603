2a92b1f3b60747a8314760f0886d29f13098d78a
SymTr <sayumi.tsuruno@gmail.com>
2024-05-09 16:52:53 +0900

    [fix] Itemcontroller 関係ない記述を一旦コメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,25 +1,25 @@
 class ItemsController < ApplicationController
-  def index
-  end
+  # def index
+  # end
 
-  def new
-    @item = Item.new
-  end
+  # def new
+  #   @item = Item.new
+  # end
 
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to @item
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @item = Item.new(item_params)
+  #   if @item.save
+  #     redirect_to @item
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  private
+  # private
 
-  def item_params
-    params.require(:item).permit(:item_name, :item_detail, :category_id,
-                                 :condition_id, :freight_id, :origin_id,
-                                 :delivery_date_id, :price, :user)
-  end
+  # def item_params
+  #   params.require(:item).permit(:item_name, :item_detail, :category_id,
+  #                                :condition_id, :freight_id, :origin_id,
+  #                                :delivery_date_id, :price, :user)
+  # end
 end
