630d607106f321fd8cdacc73dc6b825a9778c7d9
SymTr <sayumi.tsuruno@gmail.com>
2024-05-10 06:58:04 +0900

    [fix] nick name test code fixed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,16 +6,11 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
-    context '新規登録できるとき' do
-      it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
-        expect(@user).to be_valid
-      end
-    end
     context '新規登録できないとき' do
       it 'nicknameが空では登録できない' do
         @user.nick_name = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+        expect(@user.errors.full_messages).to include("Nick name can't be blank")
       end
       it 'emailが空では登録できない' do
         @user.email = ''
