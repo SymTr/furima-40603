36698c86b619f6360bf31edb095f36738b5d9f11
SymTr <sayumi.tsuruno@gmail.com>
2024-05-11 04:51:26 +0900

    Item, category_idモデルのアソシエーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-
   validates :content, presence: true
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
 end
