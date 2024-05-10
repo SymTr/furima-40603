95a19c0f6df9a179604e25f784743671bb958f50
SymTr <sayumi.tsuruno@gmail.com>
2024-05-11 06:14:34 +0900

    dropdown set!!

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,16 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one_attached :image
-  validates :content, presence: true
-
-  # ***************************************************
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category_id
 
-  validates :title, :text, presence: true
-  validates :genre_id, numericality: { other_than: 1, message: "can't be blank"} 
+  belongs_to :user
+  belongs_to :category_id
+  belongs_to :condition_id
+  belongs_to :delivery_date_id
+  belongs_to :freight_id
+  belongs_to :origin_id
+
+  has_one_attached :image
+
+  validates :content, :title, :text, :category_id, :condition_id, :delivery_date_id, :freight_id, :origin_id, presence: true
+  validates :category_id, :condition_id, :delivery_date_id, :freight_id, :origin_id, numericality: { other_than: 1, message: "can't be blank"}
+
 end
