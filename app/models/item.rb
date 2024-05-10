c53ea9e4a2f3a520fea92e72419cc13a7f1c32da
SymTr <sayumi.tsuruno@gmail.com>
2024-05-11 05:03:29 +0900

    エラーメッセージを編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,5 @@ class Item < ApplicationRecord
   belongs_to :category_id
 
   validates :title, :text, presence: true
-  validates :genre_id, numericality: { other_than: 1 } 
+  validates :genre_id, numericality: { other_than: 1, message: "can't be blank"} 
 end
