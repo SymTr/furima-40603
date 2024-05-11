97a2d1ad934cbbbb065633f278bc793fcc1ed036
SymTr <sayumi.tsuruno@gmail.com>
2024-05-11 20:41:44 +0900

    test 追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,24 @@
+# class Item < ApplicationRecord
+#   extend ActiveHash::Associations::ActiveRecordExtensions
+
+#   belongs_to :user
+#   belongs_to :category
+#   belongs_to :condition
+#   belongs_to :delivery_date
+#   belongs_to :freight
+#   belongs_to :origin
+
+#   has_one_attached :image
+
+#   validates :image, presence: true
+#   validates :category_id, :condition_id, :freight_id, :origin_id, :delivery_date_id,
+#             numericality: { other_than: 1, message: "can't be blank" }
+
+#   validates :item_name, presence: true, length: { maximum: 40 }
+#   validates :item_detail, presence: true, length: { maximum: 1000 }
+# end
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-
   belongs_to :user
   belongs_to :category
   belongs_to :condition
@@ -14,9 +32,10 @@ class Item < ApplicationRecord
   validates :category_id, :condition_id, :freight_id, :origin_id, :delivery_date_id,
             numericality: { other_than: 1, message: "can't be blank" }
 
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'Use half-width numbers' },
-                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'should be ¥300~¥9,999,999' }
-
   validates :item_name, presence: true, length: { maximum: 40 }
   validates :item_detail, presence: true, length: { maximum: 1000 }
+
+  validates :price, presence: true
+  validates :price, numericality: { only_integer: true, message: 'Use half-width numbers' }, if: Proc.new { |a| a.price.present? }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'should be ¥300~¥9,999,999' }, if: Proc.new { |a| a.price.present? && a.price.to_s =~ /\A[0-9]+\z/ }
 end
