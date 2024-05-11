8f78f3c521c7cf10b4ea50cdde4a4050c4b190b8
SymTr <sayumi.tsuruno@gmail.com>
2024-05-11 13:46:49 +0900

    FactoryBot def update

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,5 +3,15 @@ FactoryBot.define do
     item_name {Faker::Commerce.product_name}
     item_detail {Faker::Lorem.sentence}
     price {Faker::Number.between(from: 300, to: 9_999_999)}
+    category_id {Category.ids.sample}
+    condition_id {Condition.ids.sample}
+    delivery_date_id {DeliveryDate.ids.sample}
+    freight_id {Freight.ids.sample}
+    origin_id {Origin.ids.sample}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
