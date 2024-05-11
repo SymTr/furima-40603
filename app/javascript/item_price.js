08bbffcfb2e8f87c120f7703c753942886adc1a3
SymTr <sayumi.tsuruno@gmail.com>
2024-05-11 17:38:04 +0900

    sales fee setting by JS done!!!

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,10 @@
-window.addEventListener('turbo:load', () => {
-  console.log("OK");
-});
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+
+  const fee = inputValue * 0.1; //assuming the fee is 10% 
+  const feeRoundedDown = Math.floor(fee);
+
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = feeRoundedDown;
+})
