60efe0eae4509a2f18113371cea680d3f8a5d885
SymTr <sayumi.tsuruno@gmail.com>
2024-05-11 14:49:12 +0900

    FactoryBot setting done!

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,11 +3,11 @@ FactoryBot.define do
     item_name {Faker::Commerce.product_name}
     item_detail {Faker::Lorem.sentence}
     price {Faker::Number.between(from: 300, to: 9_999_999)}
-    category_id {Category.ids.sample}
-    condition_id {Condition.ids.sample}
-    delivery_date_id {DeliveryDate.ids.sample}
-    freight_id {Freight.ids.sample}
-    origin_id {Origin.ids.sample}
+    category_id { 2 } 
+    condition_id { 2 } 
+    delivery_date_id { 2 } 
+    freight_id { 2 }
+    origin_id { 2 } 
     association :user
 
     after(:build) do |item|
