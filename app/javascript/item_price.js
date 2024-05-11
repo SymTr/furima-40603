cad03605ca002585b4c502d678ef04cf843124ef
SymTr <sayumi.tsuruno@gmail.com>
2024-05-11 19:17:49 +0900

    [fix] auto calculation by JS

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,14 +1,16 @@
-const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
+window.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
 
-  const fee = inputValue * 0.1; //assuming the fee is 10% 
-  const feeRoundedDown = Math.floor(fee);
+    const fee = inputValue * 0.1; //assuming the fee is 10% 
+    const feeRoundedDown = Math.floor(fee);
 
-  const addTaxDom = document.getElementById("add-tax-price");
-  addTaxDom.innerHTML = feeRoundedDown;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = feeRoundedDown;
 
-  const profitDom =  document.getElementById("profit"); // make sure the Id matches in your HTML
-  const profitValue = inputValue - feeRoundedDown;
-  profitDom.innerHTML = Math.floor(profitValue);
+    const profitDom =  document.getElementById("profit"); // make sure the Id matches in your HTML
+    const profitValue = inputValue - feeRoundedDown;
+    profitDom.innerHTML = Math.floor(profitValue);
+  })
 })
