796a9cd71c71c5832a98817ac04efb59bc50e9d0
SymTr <sayumi.tsuruno@gmail.com>
2024-05-14 15:35:27 +0900

    association set for Destination and Purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,6 +1,6 @@
 class Purchase < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  # - has_one :destination（後で使うのでコメントアウトします）
+  has_one :destination
   belongs_to :user
   belongs_to :item
 end
