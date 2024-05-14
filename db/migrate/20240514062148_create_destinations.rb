a13ee91181a889eff05e56c70d7b4f6853bb4404
SymTr <sayumi.tsuruno@gmail.com>
2024-05-14 15:31:16 +0900

    prepare migration file for destinations and Rubcop

--- db/migrate/20240514062148_create_destinations.rb
+++ db/migrate/20240514062148_create_destinations.rb
@@ -1,7 +1,13 @@
 class CreateDestinations < ActiveRecord::Migration[7.0]
   def change
     create_table :destinations do |t|
-
+      t.string  :post_code,          null: false
+      t.integer :origin_id,          null: false
+      t.string  :city,               null: false
+      t.string  :area_number,        null: false
+      t.string  :building,           
+      t.string  :phone,              null: false
+      t.references :purchase,        null: false, foreign_key: true
       t.timestamps
     end
   end
