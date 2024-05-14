e3f7dd81be452da4119ce1f84afea118ef1b1b37
SymTr <sayumi.tsuruno@gmail.com>
2024-05-14 15:33:41 +0900

    destinations table creation

--- db/migrate/20240514062148_create_destinations.rb
+++ db/migrate/20240514062148_create_destinations.rb
@@ -5,7 +5,7 @@ class CreateDestinations < ActiveRecord::Migration[7.0]
       t.integer :origin_id,          null: false
       t.string  :city,               null: false
       t.string  :area_number,        null: false
-      t.string  :building,           
+      t.string  :building           
       t.string  :phone,              null: false
       t.references :purchase,        null: false, foreign_key: true
       t.timestamps
